8640d33611f3d4c6a9627deb2d5eb811443a9532
Raymond Lee <raymanl33@icloud.com>
2021-03-25 18:50:43 -0700

    update TodoList to array of objects and add second for loop

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,18 @@
-const TodoList = { todo: "clone repo for starter code", status:"complete" }
+const TodoList = [{todo: "clone repo for starter code"}, {status:"complete"}]
 const completedTodos = []
 
+
 for (word in TodoList) {
-    if (TodoList[word] === "complete") {
-        completedTodos.push(TodoList)
+    if (TodoList[word] === TodoList[1]) {
+        completedTodos.push(TodoList[0], TodoList[1])
+    }
+}
+
+
+for (word in completedTodos) {
+    if (completedTodos[word] === completedTodos[1]) {
+        const [{todo}] = completedTodos
+        console.log(todo)
     }
 }
 
